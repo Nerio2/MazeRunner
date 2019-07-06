cc2a92b54260a685dc1765dd64fb5d5d07fe199b
Patryk Kozarski <Patryknerio2@gmail.com>
2019-07-06 14:03:51 +0200

    save

--- Assets/Scripts/WallSpawner.cs
+++ Assets/Scripts/WallSpawner.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WallSpawner : MonoBehaviour
-{
+public class WallSpawner : MonoBehaviour {
 	public GameObject Wall;
 	public Transform Parrent;
 
+	private void Start() {
+		GameObject Start = GameObject.Find("Start");
+	}
+
 	private void OnTriggerEnter2D(Collider2D collision) {
-		Instantiate(Wall, Parrent);
+		Instantiate(Wall , Parrent);
+		if(Wall.transform.name.IndexOf('L')==0)
 		Destroy(this);
 	}
 }
