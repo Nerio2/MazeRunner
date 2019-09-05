eb0ba4b7a9a9b4f382e9d4cd8c0f79450fe8918d
Patryk Kozarski <Patryknerio2@gmail.com>
2019-09-05 20:12:50 +0200

    add Score based by distance traveled

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
@@ -9,11 +10,16 @@ public class PlayerController : MonoBehaviour {
 	public int acceleration = 80; //velocity.xy += deltaTime / acceleration               max acceleration=10, min acceleration=100
 	public int startSpeed=2;
 
+	public Text ScoreText;
+	private float score = 0;
+	private Vector2 lastPosition;
+
 	public Camera camera;
 	// Start is called before the first frame update
 	void Start() {
 		rb = GetComponent<Rigidbody2D>();
 		rb.velocity = new Vector2(0 , 2);
+		lastPosition = transform.position;
 	}
 
 	// Update is called once per frame
@@ -39,6 +45,8 @@ public class PlayerController : MonoBehaviour {
 			else
 				rb.velocity = new Vector2(0 , rb.velocity.x * -1);
 		}
-		
+		score += Vector2.Distance(transform.position , lastPosition);
+		lastPosition = transform.position;
+		ScoreText.text = "Score: " + (int)score;
 	}
 }
