f41944eb54cf5596360f13a4b1b1ea22ea928b53
Patryk Kozarski <Patryknerio2@gmail.com>
2019-09-10 19:14:45 +0200

    death scene add

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraController : MonoBehaviour {
 	public GameObject Player;
 	private Vector3 offset;
+	private float timeToEndScene = 2f;
+	private float time = 0f;
 
 	private void Start() {
 		offset = transform.position - Player.transform.position;
@@ -13,7 +14,15 @@ public class CameraController : MonoBehaviour {
 	private void LateUpdate() {
 		if ( Player != null )
 			transform.position = Player.transform.position + offset;
-		else
-			GetComponent<Camera>().orthographicSize += 0.05f;
+		else {
+			if ( time > timeToEndScene ) {
+				SceneManager.LoadScene("EndScene" , LoadSceneMode.Single);
+			} else {
+				GetComponent<Camera>().orthographicSize += 0.03f;
+				time += Time.deltaTime;
+			}
+
+
+		}
 	}
 }
