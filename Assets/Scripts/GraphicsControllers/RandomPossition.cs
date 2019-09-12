e6931f1ba8e2edcb8e30fb7c5207f6162e89fe55
Patryk Kozarski <Patryknerio2@gmail.com>
2019-09-12 23:22:26 +0200

    floor in progress/ Random generating addons on the floor

---
+++ Assets/Scripts/GraphicsControllers/RandomPossition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPossition : MonoBehaviour {
+	public int Range = 5;
+	void Start() {
+		Vector2 OldPosition = transform.position;
+		transform.position = new Vector2(Random.Range(-Range , Range) / 10f , Random.Range(-Range , Range) / 10f)+OldPosition;
+	}
+}
---
+++ Assets/Scripts/GraphicsControllers/RandomRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour {
+	public int MinRotation = 0;
+	public int MaxRotation = 360;
+
+	void Start() {
+		transform.eulerAngles = new Vector3(0 , 0 , Random.Range(MinRotation , MaxRotation));
+	}
+}
